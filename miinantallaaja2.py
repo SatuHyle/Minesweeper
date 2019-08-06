adfd6327097f595cae1cd082794c47f3683b41df
SatuHyle <44017573+SatuHyle@users.noreply.github.com>
2019-08-06 22:51:09 +0300

    Update miinantallaaja2.py

--- miinantallaaja2.py
+++ miinantallaaja2.py
@@ -12,7 +12,9 @@ painikkeet = {
 #sanakirja, johon tallentuu kentan tilanne
 tila = {
     "kentta": None,
-    "kentta2": None
+    "kentta2": None,
+    "liput": None,
+    "miinat": None
     }
 
 def kasittele_hiiri(x, y, painike, muokkaus):
@@ -20,15 +22,26 @@ def kasittele_hiiri(x, y, painike, muokkaus):
     Tätä funktiota kutsutaan kun käyttäjä klikkaa sovellusikkunaa hiirellä.
     Tulostaa hiiren sijainnin sekä painetun napin terminaaliin.
     """
+    x = int(x / 40)
+    y = int(y / 40)
+    if painike == haravasto.HIIRI_VASEN:
+        avaa_ruutu(x, y)
+    elif painike == haravasto.HIIRI_OIKEA:
+        sijoita_lippu(x, y)
     print("Hiiren nappia {} painettiin kohdassa {}, {}".format(painikkeet[painike], x, y))
 
 def miinoita(kentta, ruudut, miinat):
     """
     Asettaa kentällä N kpl miinoja satunnaisiin paikkoihin.
     """
+    miinojen_maara = []
     for i, j in random.sample(ruudut, miinat):
-        kentta[j][i] = "x"
+        kentta[i][j] = "x"
+        miinojen_maara.append((i, j))
         
+    tila["miinat"] = miinojen_maara
+    tila["kentta"] = kentta
+
 def sijoita_numerot(x, y, kentta):
     """
     Asettaa numerot miinojen ympärille
@@ -43,6 +56,29 @@ def sijoita_numerot(x, y, kentta):
     print("Ruutua {},{} ympäröi {} miinaa.".format(i, j, miinat))
     return miinat
 
+def tarkista_voitto(x, y):
+    #tarkistaa onko liput samoissa paikoissa kuin miinat
+    if set(tila["liput"]) == set(tila["miinat"]):
+        print("Voitit pelin")
+        #print("Aikaa kului: {:.2f} sekunttia".format(lopeta_kello()))
+        piirra_kentta()
+
+def sijoita_lippu(x, y):
+	# Tarkistaa onko ruutu tyhjä
+	if tila["kentta2"][x][y] == " ":
+		tila["kentta2"][x][y] = "f"
+		tila["liput"].append((x, y))
+		tarkista_voitto(x, y)
+	# Poistaa lipun
+	elif tila["kentta2"][x][y] == "f":
+		tila["kentta2"][x][y] = " "
+		tila["liput"].remove((x, y))
+		print(tila["liput"])
+	else:
+		print("Ei voi asettaa lippua")
+
+	piirra_kentta()
+
 def piirra_kentta():
     """
     Käsittelijäfunktio, joka piirtää kaksiulotteisena listana kuvatun miinakentän
@@ -51,9 +87,9 @@ def piirra_kentta():
     """
     haravasto.tyhjaa_ikkuna()
     haravasto.aloita_ruutujen_piirto()
-    for k, rivi in enumerate(tila["kentta2"]):
-        for l, ruutu in enumerate(rivi):
-            haravasto.lisaa_piirrettava_ruutu(ruutu, l * 40, k * 40)
+    for x, rivi in enumerate(tila["kentta2"]):
+        for y, ruutu in enumerate(rivi):
+            haravasto.lisaa_piirrettava_ruutu(ruutu, x * 40, y * 40)
     haravasto.piirra_ruudut()
 
 def tulvataytto(kentta, x, y):
@@ -62,19 +98,44 @@ def tulvataytto(kentta, x, y):
     täyttö aloitetaan annetusta x, y -pisteestä.
     """
     kentta2 = tila["kentta2"] #paljastaa kaikki numerot vaikkei saisi
-    if kentta[y][x] == "x": #jos ruudussa on miina, palaa loopin alkuun
+    if kentta[x][y] == "x": #jos ruudussa on miina, palaa loopin alkuun
         return
     else:    
         fill = [(x, y)] #tayta ruutu x, y 
         while fill: #tayton tapahtuessa, tee seuraavasti
             x, y = fill.pop() #removes and returns last value from the list or the given index value
-            for l in range(max(0, y - 1), min(len(kentta), y + 2)): #kaydaan lapi kaikki sarakkeet
-                for k in range(max(0, x - 1), min(len(kentta[0]), x + 2)): #kaydaan lapi kaikki rivit
-                    if kentta[l][k] == " ": #jos ruutu on tyhja
-                        fill.append((k, l)) #tayta ruutu
-                        miinat = str(sijoita_numerot(k, l, kentta))
-                        kentta[l][k] = miinat #tayttaa ruudun miinojen maaralla
-                        kentta2[l][k] = miinat
+            for nx in range(max(0, x - 1), min(len(kentta), x + 2)): #kaydaan lapi kaikki sarakkeet
+                for ny in range(max(0, y - 1), min(len(kentta[0]), y + 2)): #kaydaan lapi kaikki rivit
+                    if kentta[x][y] == " ": #jos ruutu on tyhja
+                        fill.append((nx, ny)) #tayta ruutu
+                        miinat = str(sijoita_numerot(x, y, kentta))
+                        kentta[x][y] = miinat #tayttaa ruudun miinojen maaralla
+                        #kentta2[x][y] = miinat
+
+def tarkista_havio(x, y):
+	#tarkistaa onko painetussa kohdassa miina
+	if tila["kentta"][x][y] == "x":
+		print("Hävisit pelin")
+		#print("Aikaa kului: {:.2f} sekunttia".format(lopeta_kello()))
+		tila["kentta2"] = tila["kentta"]
+		piirra_kentta()
+
+def avaa_ruutu(x, y):
+    tarkista_havio(x, y)
+	#jos on lippu, poistaa sen
+    if (x, y) == tila["liput"]:
+        tila["liput"].remove((x, y))
+        #näytä ruutu
+        tila["kentta2"][x][y] = tila["kentta"][x][y] 
+        piirra_kentta()
+
+    if tila["kentta2"][x][y] == " ":
+        if int(tila["kentta"][x][y]) > 0:
+            tila["kentta2"][x][y] = tila["kentta"][x][y] 
+        if tila["kentta"][x][y] == "0":
+            tulvataytto(x, y)
+        piirra_kentta()       
+
 def main(kentta):
     """
     Luo sovellusikkunan ja asettaa käsittelijäfunktion hiiren klikkauksille.
@@ -97,23 +158,24 @@ if __name__ == "__main__":
     
     kentta = [] #luodaan kentta
     kentta2 = [] #luodaan toinen kentta, jonka avulla peitetaan miinat
-    for rivi in range(kokoy): #korkeus                           
+    for rivi in range(kokox): #korkeus                           
         kentta.append([])
         kentta2.append([])
-        for sarake in range(kokox): #leveys
+        for sarake in range(kokoy): #leveys
             kentta[-1].append(" ")
             kentta2[-1].append(" ")
 
     jaljella = [] #lista vapaista ruuduista
     jaljella2 = []
-    for x in range(kokox): #leveys
-        for y in range(kokoy): #korkeus        
+    for x in range(kokoy): #leveys
+        for y in range(kokox): #korkeus        
             jaljella.append((x, y)) 
-            jaljella2.append((x, y))    
-    
+            jaljella2.append((x, y))
+
+    tila["liput"] = []
     tila["kentta"] = kentta
     tila["kentta2"] = kentta2
-
+    
     miinoita(kentta, jaljella, miinat)
     tulvataytto(kentta, xkoor, ykoor)
     main(kentta)
